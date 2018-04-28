695864911d98e70b91944b785aeaa1fcca9b9ddc
Ninjas In The Machine <developers@ninjasitm.com>
2018-04-28 18:59:47 -0400

    Fixes for cms config.toml file

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,11 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ServicePagePreview from './preview-templates/ServicePagePreview'
+import ContactPagePreview from './preview-templates/ContactPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('contact', ContactPagePreview)
+CMS.registerPreviewTemplate('service', ServicePagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
